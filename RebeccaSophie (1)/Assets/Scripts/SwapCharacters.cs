b6b38b74bfa58df7283ead1fc87f93bd4513364f
sophielynch221 <100151278+sophielynch221@users.noreply.github.com>
2022-03-14 11:14:29 +0000

    mainMenu

--- RebeccaSophie (1)/Assets/Scripts/SwapCharacters.cs
+++ RebeccaSophie (1)/Assets/Scripts/SwapCharacters.cs
@@ -11,7 +11,7 @@ public class SwapCharacters : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        swap = true;
     }
 
     // Update is called once per frame
@@ -19,22 +19,52 @@ public class SwapCharacters : MonoBehaviour
     {
         if (swap)
         {
+            //so penguin would follow
             penguin.GetComponent<NavMeshAgent>().enabled = true;
-            Girl.GetComponent<NavMeshAgent>().enabled = false;
             penguin.GetComponent<Follow>().enabled = false;
+
+            //so girl isnt following
+            Girl.GetComponent<NavMeshAgent>().enabled = false;
             Girl.GetComponent<Follow>().enabled = true;
+
+            //girl can run
+            Girl.GetComponent<NavMeshAgent>().enabled = false;
+            Girl.GetComponent<Follow>().enabled = true;
+          //  Girl.GetComponent<ThirdPersonController>().enabled = true;
+            Girl.GetComponent<CharacterController>().enabled = true;
+
+            //penguin cant run
+            penguin.GetComponent<NavMeshAgent>().enabled = true;
+            penguin.GetComponent<Follow>().enabled = false;
+           // penguin.GetComponent<ThirdPersonController>().enabled = false;
+            penguin.GetComponent<CharacterController>().enabled = false; 
         }
 
         if (!swap)
         {
+            // so penguin wont follow
             penguin.GetComponent<NavMeshAgent>().enabled = false;
-            Girl.GetComponent<NavMeshAgent>().enabled = true;
             penguin.GetComponent<Follow>().enabled = true;
+
+            // so girl wont run
+            Girl.GetComponent<NavMeshAgent>().enabled = true;
             Girl.GetComponent<Follow>().enabled = false;
+
+            //penguin can run
+            penguin.GetComponent<NavMeshAgent>().enabled = false;
+            penguin.GetComponent<Follow>().enabled = true;
+            //penguin.GetComponent<ThirdPersonController>().enabled = true;
+            penguin.GetComponent<CharacterController>().enabled = true;
+
+            //girl cant run
+            Girl.GetComponent<NavMeshAgent>().enabled = true;
+            Girl.GetComponent<Follow>().enabled = false;
+          //  Girl.GetComponent<ThirdPersonController>().enabled = false;
+            Girl.GetComponent<CharacterController>().enabled = false;
         }
     }
 
-    void checkcharacters()
+    void OnChangecharacter()
     {
         swap = !swap;
     }
