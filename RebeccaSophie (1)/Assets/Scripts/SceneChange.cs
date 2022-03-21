2833727393a4826d1e7016b2a046d711cdc0a7e1
sophielynch221 <100151278+sophielynch221@users.noreply.github.com>
2022-03-21 15:23:44 +0000

    Stuff

---
+++ RebeccaSophie (1)/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class SceneChange : MonoBehaviour
+{
+    public UnityEvent changeScene;
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            changeScene.Invoke();
+        }
+    }
+}
